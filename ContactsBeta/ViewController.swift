9d60b4d029ad69d2337181cfeaaf9d5c9f20b8af
Konstantine Tsirgvava <konstantine@Konstantines-iMac.local>
2022-11-26 20:46:18 +0400

    Initial Commit

---
+++ ContactsBeta/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ContactsBeta
+//
+//  Created by Konstantine Tsirgvava on 11/26/22.
+//  Copyright © 2022 Konstantine Tsirgvava. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
